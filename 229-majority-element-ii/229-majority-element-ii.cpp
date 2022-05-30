9cff21b01337b596723ce0f23dee5eec18153f6e
Manoj loul <choudharymanojloul844@gmail.com>
2022-05-30 19:13:21 +0530

    Time: 26 ms (17.66%), Space: 15.7 MB (82.45%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();++i)
-            mp[nums[i]]++;
-        
-        int n=nums.size();
-        vector<int> v;
-        for(auto it:mp)
-        {
-            if(it.second>n/3)
-                v.push_back(it.first);
-        }
-        return v;
+    vector<int> majorityElement(vector<int>& arr) {
+      int num1=-1,num2=-1,c1=0,c2=0;
+
+   for(int i=0;i<arr.size();++i)
+   {
+       if(num1==arr[i]) c1++;
+       else if(num2==arr[i]) c2++;
+       else if(c1==0)
+       {
+           num1=arr[i];
+           c1=1;
+       }
+       else if(c2==0)
+       {
+           num2=arr[i];
+           c2=1;
+       }
+       else
+       {
+           c1--;
+           c2--;
+       }
+   }
+cout<<num1<<" "<<num2<<endl;
+   c1=0,c2=0;
+   for(int i=0;i<arr.size();++i)
+   {
+       if(num1==arr[i]) c1++;
+       else if(num2==arr[i]) c2++;
+   }
+   
+   if(c1>arr.size()/3 && c2>arr.size()/3) return {num1,num2};
+   else if(c1>arr.size()/3) return {num1};
+   else if(c2>arr.size()/3) return {num2};
+   return {};
     }
 };
