bb40ba8d8de517dac7282cfe34d5a10e4018688a
Manoj loul <choudharymanojloul844@gmail.com>
2022-06-05 04:17:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (6.12%) - LeetHub

---
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int l=0,a=0;
+        
+        for(int i=0;i<s.size();++i)
+        {
+            if(s[i]=='A')
+            {
+                a++;
+                l=0;
+            }
+            else if(s[i]=='L')
+            {
+                l++;
+            }
+            else
+            {
+                l=0;
+            }
+            if(l>=3) return false;
+        }
+        if(a>=2) return false;
+        return true;
+    }
+};
