feb27f9e2182ecb76af529bc2c8f257d5bd7b182
Manoj loul <choudharymanojloul844@gmail.com>
2022-06-16 14:47:03 +0530

    Time: 245 ms (45.94%), Space: 238.3 MB (15.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    pair<int,int> ispal(string s,int i,int j)
+    pair<int,int> solve(string s,int i,int j)
     {
-        if(s[i]!=s[j]) return {i,j-1};
         while(i>=0 && j<s.size())
         {
-            if(s[i]!=s[j]) break;
-            i--;
-            j++;
+            if(s[i]==s[j])
+            {
+                i--;
+                j++;
+            }
+            else break;
         }
-        i++;
-        j--;
-        return {i,j};
+        return {i+1,j-1};
     }
     string longestPalindrome(string s) {
         if(s.size()==1) return s;
@@ -19,24 +19,16 @@ public:
         
         for(int i=0;i<s.size()-1;++i)
         {
-            pair<int,int> p1=ispal(s,i,i);
-            pair<int,int> p2=ispal(s,i,i+1);
-            if(p1.second-p1.first>p2.second-p2.first)
-            {
-                int e=p1.second;
-                int st=p1.first;
-                if(e-st+1>ans.size())
-                    ans=s.substr(st,e-st+1);
-            }
-            else
-            {
-                int e=p2.second;
-                int st=p2.first;
-                if(e-st+1>ans.size())
-                    ans=s.substr(st,e-st+1);
-            }
-           // cout<<ans<<endl;
+            pair<int,int> p1=solve(s,i,i);
+            pair<int,int> p2=solve(s,i,i+1);
+            
+            if(p1.second-p1.first+1>ans.size())
+                ans=s.substr(p1.first,p1.second-p1.first+1);
+            
+            if(p2.second-p2.first+1>ans.size())
+                ans=s.substr(p2.first,p2.second-p2.first+1);
         }
+        
         return ans;
     }
 };
