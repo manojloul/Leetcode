63a5212ef0dc4681695dfa277eb56f6f42093f22
Manoj loul <choudharymanojloul844@gmail.com>
2022-06-23 05:54:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.19%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long nn=abs(n);
+        double ans=1.0;
+        while(nn>0)
+        {
+            if(nn%2==0)
+            {
+                x=x*x;
+                nn/=2;
+            }
+            else
+            {
+                ans=ans*x;
+                nn-=1;
+            }
+        }
+        if(n<0) ans=(double)1/(double)ans;
+        return ans;
     }
 };
