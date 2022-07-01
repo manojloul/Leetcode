197dfdfcd936b696926e823c7e5fd6a624124bb0
Manoj loul <choudharymanojloul844@gmail.com>
2022-07-02 02:01:57 +0530

    Time: 117 ms (27.87%), Space: 22.4 MB (56.27%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-        int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
         
-        for(int i=0;i<grid.size();++i)
+        vector<int> row(n,0),col(m,0);
+        
+        for(int i=0;i<n;++i)
         {
-            int f=0;
-            for(int j=0;j<grid[0].size();++j)
+            for(int j=0;j<m;++j)
             {
                 if(grid[i][j]==1)
-                    f++;
-            }
-            if(f>1){
-             for(int j=0;j<grid[0].size();++j)
-                 if(grid[i][j]==1) grid[i][j]=2;
-            }
-        }
-        for(int i=0;i<grid[0].size();++i)
-        {
-            int f=0;
-            for(int j=0;j<grid.size();++j)
-            {
-                if(grid[j][i]==1 ||grid[j][i]==2)
-                    f++;
-            }
-            if(f>1){
-             for(int j=0;j<grid.size();++j)
-                 if(grid[j][i]==1) grid[j][i]=2;
+                {
+                    row[i]++;
+                    col[j]++;
+                }
             }
         }
-        for(int i=0;i<grid.size();++i)
+        
+        int ans=0;
+        for(int i=0;i<n;++i)
         {
-            for(int j=0;j<grid[0].size();++j)
+            for(int j=0;j<m;++j)
             {
-                if(grid[i][j]==2)
-                    ans++;
+                if(grid[i][j]==1 && (col[j]>1 || row[i]>1)) ans++;
             }
         }
         return ans;
+        
     }
 };
