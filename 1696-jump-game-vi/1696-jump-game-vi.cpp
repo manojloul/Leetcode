687718ca11d1db1b09a58e0660850d4b1232a1d8
Manoj loul <choudharymanojloul844@gmail.com>
2022-07-10 04:26:42 +0530

    Time: 239 ms (76.57%), Space: 77.4 MB (93.81%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        deque<int> q;
+        q.push_back(0);
+        for(int i=1;i<nums.size();++i)
+        {
+            if(q.front()<i-k) q.pop_front();
+            
+            nums[i]+=nums[q.front()];
+            
+            while(!q.empty() && nums[q.back()]<=nums[i]) q.pop_back();
+            
+            q.push_back(i);
+            
+        }
+        return nums[nums.size()-1];
+    }
+};
