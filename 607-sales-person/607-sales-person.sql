4300052a8cad5fa3187f618eb51a68706474176b
Manoj loul <choudharymanojloul844@gmail.com>
2022-08-21 17:03:55 +0530

    Time: 2149 ms (27.29%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select name from SalesPerson where name not in(
 select s.name from SalesPerson s where
-s.sales_id in ( select o.sales_id from Orders o where o.com_id in 
-             (select c.com_id from Company c where c.name = "RED")))
+s.sales_id not in ( select o.sales_id from Orders o where o.com_id in 
+             (select c.com_id from Company c where c.name = "RED"))
