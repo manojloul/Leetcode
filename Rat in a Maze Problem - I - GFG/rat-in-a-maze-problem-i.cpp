963a4ed1ae1f14aabfd6aa54f229866060f4e8e5
Manoj loul <choudharymanojloul844@gmail.com>
2023-06-24 12:35:07 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -1,72 +1,49 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 // User function template for C++
 
 class Solution{
-    public:vector<string> ans;
-    bool isvalid(vector<vector<int>> &mat,int i,int j)
+    public:
+    vector<string> ans;
+    bool is_valid(vector<vector<int>> m ,int n,int i,int j)
     {
-        int n=mat.size();
         if(i<0 || i>=n || j<0 || j>=n) return false;
-        if(mat[i][j]==0)
-        return false;
-        return true;
+        return m[i][j] == 1;
     }
-    void solve(vector<vector<int>> &rat,int n,int i,int  j,string &s)
+    void solve(vector<vector<int>>& m ,int n, int i,int j, string st)
     {
-        if(i==n-1 && j==n-1)
+        if( i == n-1 && j == n-1)
         {
-            //cout<<s<<endl;
-            ans.push_back(s);
+            ans.push_back(st);
             return;
         }
+        else if( i==n) return;
         
-        if(isvalid(rat,i,j-1))
+        int row_change[4]={-1,1,0,0};
+        int col_change[4]={0,0,-1,1};
+        char dir[4] = {'U','D','L','R'};
+        
+        for(int p=0;p<4;++p)
         {
-            rat[i][j]=0;
-            s.push_back('L');
-            solve(rat,n,i,j-1,s);
-            s.pop_back();
-            rat[i][j]=1;
-        }
-       // cout<<isvalid(rat,i+1,j)<<endl;
-         if(isvalid(rat,i,j+1))
-        {
-            rat[i][j]=0;
-            s.push_back('R');
-            solve(rat,n,i,j+1,s);
-            s.pop_back();
-            rat[i][j]=1;
-        }
-         if(isvalid(rat,i+1,j))
-        {
-            rat[i][j]=0;
-            s.push_back('D');
-            solve(rat,n,i+1,j,s);
-            s.pop_back();
-            rat[i][j]=1;
-        }
-         if(isvalid(rat,i-1,j))
-        {
-            rat[i][j]=0;
-            s.push_back('U');
-            solve(rat,n,i-1,j,s);
-            s.pop_back();
-            rat[i][j]=1;
+            if(is_valid(m,n,i+row_change[p],j+col_change[p]))
+            {
+                st+=dir[p];
+                m[i][j]=-1;
+                solve(m,n,i+row_change[p],j+col_change[p],st);
+                st.pop_back();
+                m[i][j]=1;
+            }
         }
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
-        vector<string> p;
-        if(m[0][0]==0 || m[n-1][n-1]==0) return p;
-        string s;
-        solve(m,n,0,0,s);
-        if(ans.size()==0) return p;
+        if(m[0][0] == 0 || m[n-1][n-1] == 0) return {"-1"};
+        solve(m,n,0,0,"");
         return ans;
     }
 };
@@ -74,7 +51,7 @@ class Solution{
     
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -98,4 +75,5 @@ int main() {
         cout << endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
